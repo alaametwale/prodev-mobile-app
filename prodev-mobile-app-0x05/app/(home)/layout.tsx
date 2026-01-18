ec212466d06a3e3e99af00532cd8cf5d2f0ac05a
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:34:59 +0200

    Create layout.tsx

---
+++ prodev-mobile-app-0x05/app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import { Tabs } from "expo-router";
+
+export default function HomeLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="search" options={{ title: "Search" }} />
+      <Tabs.Screen name="saved" options={{ title: "Saved" }} />
+      <Tabs.Screen name="inbox" options={{ title: "Inbox" }} />
+      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+    </Tabs>
+  );
+}
