85603c4ad15bad547b473bf03fbc7f3cb3e5f41c
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:55:27 +0200

    Update _layout.tsx

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -1,15 +1,49 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { AntDesign, Feather, EvilIcons, Ionicons, FontAwesome } from "@expo/vector-icons";
 
 export default function RootLayout() {
   return (
-    <Stack
+    <Tabs
       screenOptions={{
+        tabBarActiveTintColor: "#34967C",
         headerShown: false,
       }}
     >
-      <Stack.Screen name="/" />
-      <Stack.Screen name="join" />
-      <Stack.Screen name="signin" />
-    </Stack>
+      <Tabs.Screen
+        name="(home)/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(home)/search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(home)/saved"
+        options={{
+          title: "Saved",
+          tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(home)/inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="(home)/profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color={color} />,
+        }}
+      />
+    </Tabs>
   );
 }
