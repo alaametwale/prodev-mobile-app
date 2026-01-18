d128ff6c499b97c611701e182cc01eed32bbc4be
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:23:50 +0200

    Create profile.tsx

---
+++ prodev-mobile-app-0x05/app/(home)/profile.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { mainStyles } from "../../styles/_mainstyle";
+
+export default function Profile() {
+  return (
+    <View style={mainStyles.container}>
+      <Text style={mainStyles.title}>Profile</Text>
+    </View>
+  );
+}
