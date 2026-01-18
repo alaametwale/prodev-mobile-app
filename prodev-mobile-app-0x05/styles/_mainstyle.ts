44bb977921c035c8d3a533aa1b1532eb0508e2c4
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:24:42 +0200

    Create _mainstyle.ts

---
+++ prodev-mobile-app-0x05/styles/_mainstyle.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const mainStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "600",
+  },
+});
+
+export { mainStyles };
