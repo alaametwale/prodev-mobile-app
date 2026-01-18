972d8333e21acf94873722018a86519230b0fdaf
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:22:39 +0200

    Create saved.tsx

---
+++ prodev-mobile-app-0x05/app/(home)/saved.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { mainStyles } from "../../styles/_mainstyle";
+
+export default function Saved() {
+  return (
+    <View style={mainStyles.container}>
+      <Text style={mainStyles.title}>Saved</Text>
+    </View>
+  );
+}
