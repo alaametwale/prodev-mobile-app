606ebfd38cbff1c3626fabfa40e2bad13d4cb6ec
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:31:42 +0200

    Update and rename layout.tsx to _layout.tsx

--- prodev-mobile-app-0x05/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x05/app/(home)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from "expo-router";
 
 export default function HomeLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="search" options={{ title: "Search" }} />
       <Tabs.Screen name="saved" options={{ title: "Saved" }} />
