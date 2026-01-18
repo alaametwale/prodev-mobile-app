5ee3bc5b2f6f60f6606b3aedb5d2f9a16957b841
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 02:22:11 +0200

    Create search.tsx

---
+++ prodev-mobile-app-0x05/app/(home)/search.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { mainStyles } from "../../styles/_mainstyle";
+
+export default function Search() {
+  return (
+    <View style={mainStyles.container}>
+      <Text style={mainStyles.title}>Search</Text>
+    </View>
+  );
+}
